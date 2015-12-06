d792d34fd93020e75254722d9991f7f37881aa42
Michael Coyne <mikeycgto@gmail.com>
2015-12-06 15:38:09 -0500

    Continue to implement

--- flask_cent.py
+++ flask_cent.py
@@ -4,87 +4,93 @@ __author__ = "BreakBase.com"
 __license__ = 'MIT'
 __copyright__ = "(c) 2015 by BreakBase.com"
 
+import blinker
 import logging
 
 from cent.core import Client as CentCoreClient
+
 from flask import current_app, _app_ctx_stack as stack
-from flask.ext.login import current_user
+from contextlib import contextmanager
 
 __all__ = ['CentClient']
 
 log = logging.getLogger(__name__)
 
-class CentClient(object):
+class Message(object):
+     def __init__(self, *args):
+         self.args = args
+
+     def command():
+         return command
+
+class Unsubscribe(Message):
+    command = 'unsubscribe'
+
+class Disconnect(Message):
+    command = 'disconnect'
+
+class Publish(Message):
+    command = 'publish'
+
+
+class ClientContext(object):
+    @contextmanager
+    def record_messages(self):
+        if not message_sent: # signals required
+            raise RuntimeError("blicker must be installed")
+
+        messages = []
+
+        def record(message, **extras):
+            messages.append(message)
+
+        message_sent.connect(record)
+
+        try:
+            yield messages
+        finally:
+            message_sent.disconnect(record)
+
+    def send(self, message):
+        fn = getattr(self.client, message.command)
+
+        if fn is not None:
+            err = fn(*message.args)
+
+            print err
+
+            if err is None:
+                message_sent.send(message)
+            else:
+                message_error.send(message)
+
+class CentClient(ClientContext):
     def __init__(self, app=None):
         self.app = app
 
         if app is not None:
-            self.state = self.init_app(app)
-        else:
-            self.state = None
+            self.init_cent_client(app.config, app.debug, app.testing)
 
-        # TODO read from configuration/env vars
-        #self.client = Client("http://localhost:8000", "development-secret")
-
-    def init_app(self, app):
-        client = self.init_cent_client(app.config, app.debug, app.testing)
-
-        app.extensions = getattr(app, 'extensions', {})
-        app.extensions['cent'] = client
-
-        return client
+            app.extensions = getattr(app, 'extensions', {})
+            app.extensions['cent'] = self.client
 
     def init_cent_client(self, config, debug=False, testing=False):
         host = config.get('FLASK_CENT_HOST', 'localhost')
         port = config.get('FLASK_CENT_PORT', '8000')
+        protocol = config.get('FLASK_CENT_PROTOCOL', 'http')
         secret = config.get('FLASH_CENT_SECRET', None)
 
-        return Client(host, port, secret)
-
-class CentClientContext(object):
-    @contextmanager
-    def record_messages(self):
-        if not messages_disptached:
-            raise RuntimeError("blicker must be installed")
-
-        published_messages = []
-
-    #def self(self, message):
-
-
-class Client(CentClientContext):
-    def __init__(self, secret, host, port, protocol='http'):
         if secret is None:
             raise RuntimeError("secret is required")
 
-        self.client = CentCoreClient("%s://%s:%s" % protocol, host, port, secret)
+        self.client = CentCoreClient("%s://%s:%s" % (protocol, host, port), secret)
 
-    #def publish(channel_id, data):
-        #return self.client.publish(channel_id, data)
+signals = blinker.Namespace()
 
-    #def unsubscribe(user_id):
-        #return self.client.unsubscribe(user_id)
+message_sent = signals.signal('message-sent', doc="""
+    Signal is sent when a message is successfully sent via Cent.
+""")
 
-    #def disconnect(user_id):
-        #return self.client.disconnect(user_id)
-
-    #def stats():
-        #return client.stats()
-
-class Message(object):
-     def __init__(self, cid):
-         self.cid = cid
-
-     def command:
-         return command
-
-class Unsubscribe(Message):
-
-class Disconnect(Message):
-    pass
-
-class Publish(Message):
-     def __init__(self, cid, contents):
-         super(self, cid)
-
-         self.contents = contents
+message_error = signals.signal('message-error', doc="""
+    Signal is sent when a message fails to send via Cent.
+""")
--- tests.py
+++ tests.py
@@ -5,27 +5,63 @@ import email
 import unittest
 import time
 import re
-import mock
 from contextlib import contextmanager
 
 from email.header import Header
 from email import charset
 
 from flask import Flask
-from flash_cent import CentClient, Publish, Unsubscribe, Disconnect
-from speaklater import make_lazy_string
+from flask_cent import CentClient, Publish, Unsubscribe, Disconnect
 
 
 class TestCase(unittest.TestCase):
     def setUp(self):
         self.app = Flask(__name__)
         self.app.config.from_object(self)
-        self.app.config.set('FLASH_CENT_SECRET', 'flask-cent-test-secret')
+        self.app.config['FLASH_CENT_SECRET'] = 'flask-cent-test-secret'
 
         self.cent = CentClient(self.app)
 
         self.ctx = self.app.test_request_context()
         self.ctx.push()
 
-    def teardown
+    def teardown():
         self.ctx.pop()
+
+class TestPublish(TestCase):
+    def setUp(self):
+        self.msg = Publish('channel_id', { 'key': 'value' })
+
+    def test_initialize(self):
+        self.assertEquals(self.msg.args, ('channel_id', { 'key': 'value' }))
+
+    def test_command(self):
+        self.assertEquals(self.msg.command, 'publish')
+
+class TestDisconnect(TestCase):
+    def setUp(self):
+        self.msg = Disconnect('user_id')
+
+    def test_initialize(self):
+        self.assertEquals(self.msg.args, ('user_id',))
+
+    def test_command(self):
+        self.assertEquals(self.msg.command, 'disconnect')
+
+class TestUnsubscribe(TestCase):
+    def setUp(self):
+        self.msg = Unsubscribe('user_id')
+
+    def test_initialize(self):
+        self.assertEquals(self.msg.args, ('user_id',))
+
+    def test_command(self):
+        self.assertEquals(self.msg.command, 'unsubscribe')
+
+class TestCent(TestCase):
+    def test_send(self):
+        with self.cent.record_messages() as messages:
+            msg = Publish('channel_id', {'key': 'value'})
+
+            self.cent.send(msg)
+            self.assertEquals(len(messages), 1)
